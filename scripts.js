3e278830a7ef5f77399925cd6d6897f92e025d55
lufaxlufa <seu-email>
2025-07-09 23:40:08 -0300

    projeto-starbuks

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+
+let circulo = document.querySelector(".circulo")
+let imagem = document.querySelector(".starbucks")
+
+function trocarCor(cor) {
+    circulo.style.background = cor
+}
+function trocarimagem(img){
+    imagem.src = img
+}
